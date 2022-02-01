b4f6599d72c6f9d10e80921f22af54e88c367b7b
Sylvain Rabot <sylvain@abstraction.fr>
2022-02-01 20:57:24 +0100

    Allow time.Duration or int for timeouts
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- dialer.go
+++ dialer.go
@@ -3,6 +3,7 @@ package quickfix
 import (
 	"fmt"
 	"net"
+	"time"
 
 	"golang.org/x/net/proxy"
 
@@ -12,8 +13,16 @@ import (
 func loadDialerConfig(settings *SessionSettings) (dialer proxy.Dialer, err error) {
 	stdDialer := &net.Dialer{}
 	if settings.HasSetting(config.SocketTimeout) {
-		if stdDialer.Timeout, err = settings.DurationSetting(config.SocketTimeout); err != nil {
-			return
+		timeout, err := settings.DurationSetting(config.SocketTimeout)
+		if err != nil {
+			timeoutInt, err := settings.IntSetting(config.SocketTimeout)
+			if err != nil {
+				return stdDialer, err
+			}
+
+			stdDialer.Timeout = time.Duration(timeoutInt) * time.Second
+		} else {
+			stdDialer.Timeout = timeout
 		}
 	}
 	dialer = stdDialer
